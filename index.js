fb763a630ec79e0ddde46440794a7701543212f6
Kaua-Gripa-firme <141835214+Kaua-Gripa-firme@users.noreply.github.com>
2023-08-21 11:53:41 -0300

    aula-9

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let array = ["1","2","3","4","5","6"]
+
+array.push ("7")
+
+console.log ("Tamanho Da Array/Lista: ", array.length)
+
+console.log (array)
+
+array.splice (3,2)
+
+console.log ("Tamanho Da Array/Lista: ", array.length)
+
+console.log (array)
+
